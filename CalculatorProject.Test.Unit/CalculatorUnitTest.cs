bb8ba1d5127651ed908410812bfbda14c7fe1902
megsgaard <mathias.egsgaard@post.au.dk>
2019-02-05 12:58:58 +0100

    Tilføjet en testcase til add

--- CalculatorProject.Test.Unit/CalculatorUnitTest.cs
+++ CalculatorProject.Test.Unit/CalculatorUnitTest.cs
@@ -9,14 +9,14 @@ using NUnit.Framework;
 namespace CalculatorProject.Test.Unit
 {
    [TestFixture]
-    public class CalculatorUnitTest
+   public class CalculatorUnitTest
    {
       private Calculator uut;
 
       [SetUp]
       public void SetUp()
       {
-         uut= new Calculator();
+         uut = new Calculator();
       }
 
       [TestCase(2.1, 3.4, 5.5)]
@@ -24,10 +24,11 @@ namespace CalculatorProject.Test.Unit
       [TestCase(-2.1, -3.4, -5.5)]
       [TestCase(0, 2.1, 2.1)]
       [TestCase(-2.1, 0, -2.1)]
-      [TestCase(2,4,6)]
+      [TestCase(2, 4, 6)]
+      [TestCase(0, 4, 4)]
       public void Add_AddNumber1AndNumber2_EqualsNumber3(double number1, double number2, double number3)
       {
-         Assert.That(Math.Round(uut.Add(number1, number2),2),Is.EqualTo(number3));
+         Assert.That(Math.Round(uut.Add(number1, number2), 2), Is.EqualTo(number3));
       }
 
       [TestCase(2.1, 3.4, -1.3)]
@@ -51,7 +52,7 @@ namespace CalculatorProject.Test.Unit
       }
 
       [TestCase(2.1, 3.4, 12.461)]
-      [TestCase(-2, 2, 4)] 
+      [TestCase(-2, 2, 4)]
       [TestCase(2.1, -3.4, 0.08)]
       [TestCase(0, 2.1, 0)]
       [TestCase(-2.1, 0, 1)]
@@ -60,49 +61,49 @@ namespace CalculatorProject.Test.Unit
          Assert.That(Math.Round(uut.Power(number1, number2), 3), Is.EqualTo(number3));
       }
 
-       [TestCase(4,2,2)]
-       [TestCase(-5,-5,1)]
-       [TestCase(-5,1,-5)]
-       [TestCase(10,-2,-5)]
-       [TestCase(0,2,0)]
-       public void Divide_DivideNumber1WithNumber2_EqualsNumber3(double number1, double number2, double number3)
-       {
-           Assert.That(Math.Round(uut.Divide(number1, number2), 3), Is.EqualTo(number3));
-       }
+      [TestCase(4, 2, 2)]
+      [TestCase(-5, -5, 1)]
+      [TestCase(-5, 1, -5)]
+      [TestCase(10, -2, -5)]
+      [TestCase(0, 2, 0)]
+      public void Divide_DivideNumber1WithNumber2_EqualsNumber3(double number1, double number2, double number3)
+      {
+         Assert.That(Math.Round(uut.Divide(number1, number2), 3), Is.EqualTo(number3));
+      }
 
-       [Test]
-       public void Divide_DivisionByZero_ThrowsException()
-       {
-           var ex = Assert.Catch<DivideByZeroException>(() => uut.Divide(6, 0));
-           StringAssert.Contains("Division by zero not possible",ex.Message);
-       }
+      [Test]
+      public void Divide_DivisionByZero_ThrowsException()
+      {
+         var ex = Assert.Catch<DivideByZeroException>(() => uut.Divide(6, 0));
+         StringAssert.Contains("Division by zero not possible", ex.Message);
+      }
 
       [Test]
       public void Accumulator_AddIsCalled_AccumulatorIsEqualToResultOfAddMethod()
       {
          uut.Add(3, 4);
-         Assert.That(uut.Accumulator,Is.EqualTo(uut.Add(3,4)));
+         Assert.That(uut.Accumulator, Is.EqualTo(uut.Add(3, 4)));
       }
 
       [Test]
       public void Accumulator_SubtractIsCalled_AccumulatorIsEqualToResultOfSubtractMethod()
       {
          uut.Subtract(5, 1);
-         Assert.That(uut.Accumulator,Is.EqualTo(uut.Subtract(5,1)));
+         Assert.That(uut.Accumulator, Is.EqualTo(uut.Subtract(5, 1)));
       }
 
       [Test]
       public void Accumulator_MultiplyIsCalled_AccumulatorIsEqualToResultOfMultiplyMethod()
       {
          uut.Multiply(3, 55);
-         Assert.That(uut.Accumulator,Is.EqualTo(uut.Multiply(3,55)));
+         Assert.That(uut.Accumulator, Is.EqualTo(uut.Multiply(3, 55)));
       }
 
       [Test]
       public void Accumulator_PowerIsCalled_AccumulatorIsEqualToResultOfPowerMethod()
       {
          uut.Power(2, -4);
-         Assert.That(uut.Accumulator,Is.EqualTo(uut.Power(2,-4)));
+         Assert.That(uut.Accumulator, Is.EqualTo(uut.Power(2, -4)));
       }
 
       [Test]
@@ -110,7 +111,7 @@ namespace CalculatorProject.Test.Unit
       {
          uut.Add(3, 6);
          uut.Subtract(8, 6);
-         Assert.That(uut.Accumulator,Is.EqualTo(uut.Subtract(8,6)));
+         Assert.That(uut.Accumulator, Is.EqualTo(uut.Subtract(8, 6)));
       }
 
       [Test]
